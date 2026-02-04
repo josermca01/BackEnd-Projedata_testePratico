e4eedac1946be3d1166fd010d0684c2cdef9bd86
josermca01 <josermca01@gmail.com>
2026-02-04 20:10:57 -0300

    classes atributes

--- src/main/java/com/example/backend_projedata/model/RawMaterial.java
+++ src/main/java/com/example/backend_projedata/model/RawMaterial.java
@@ -1,5 +1,26 @@
 package com.example.backend_projedata.model;
 
-public class RawMaterial {
+import java.io.Serializable;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RawMaterial implements Serializable{
+
+    private static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double quantity;
 }
