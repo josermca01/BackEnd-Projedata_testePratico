c481f4f7fcae04f6848cb88e782c5449c24ced07
josermca01 <josermca01@gmail.com>
2026-02-04 19:51:55 -0300

    start

---
+++ src/main/java/com/example/backend_projedata/BackendProjedataApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_projedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjedataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_projedata/BackendProjedataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_projedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjedataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
